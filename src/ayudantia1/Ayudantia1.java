2e5c561847f543e64e40fbbd4e27db42f2cf992f
migfe <migfe@192.168.40.46>
2018-04-06 11:34:08 -0300

    nuevo por

---
+++ src/ayudantia1/Ayudantia1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayudantia1;
+
+/**
+ *
+ * @author migfe
+ */
+public class Ayudantia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
